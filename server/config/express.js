380708722594794e7a63dcbcfe64c5ca7a0332b6
simboyy <smkorera@gmail.com>
2016-04-27 15:47:13 +0200

    update mongo environment

--- server/config/express.js
+++ server/config/express.js
@@ -48,12 +48,17 @@ module.exports = function(app) {
   
   if ('production' === env) {
 
-    // app.use(require('connect-livereload')());
-    app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', path.join(config.root, 'client'));
+    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    app.use(express.static(path.join(config.root, 'public')));
+    app.set('appPath', path.join(config.root, 'public'));
     app.use(morgan('dev'));
-    app.use(errorHandler()); // Error handler - has to be last
+
+    // app.use(require('connect-livereload')());
+    // app.use(express.static(path.join(config.root, '.tmp')));
+    // app.use(express.static(path.join(config.root, 'client')));
+    // app.set('appPath', path.join(config.root, 'client'));
+    // app.use(morgan('dev'));
+    // app.use(errorHandler()); // Error handler - has to be last
     // app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     // app.use(express.static(path.join(config.root, 'public')));
     // app.set('appPath', path.join(config.root, 'public'));
